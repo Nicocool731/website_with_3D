88456225c466027fe2939944f540dfad31998218
Nicocool731 <kuzumi.sakuri731@gmail.com>
2026-07-20 15:34:36 +0200

    gtwt

---
+++ soundscript.js
@@ -0,0 +1,14 @@
+
+
+
+
+const image = document.getElementById("shrek");
+const sound = new Audio("./music-for-videos-donx27t-say-goodbye-funny-electro-swing-song-151282.mp3");
+
+image.addEventListener("click", () => {
+    sound.currentTime = 0;
+    sound.play().catch(error => {
+        console.error("The sound could not be played:", error);
+    });
+});
+
