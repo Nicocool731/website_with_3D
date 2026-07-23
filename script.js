f276b6967f6e1766b9c4f376400beb8a96380bcc
Nicocool731 <kuzumi.sakuri731@gmail.com>
2026-07-23 10:13:10 +0200

    rths

--- script.js
+++ script.js
@@ -1,13 +1,25 @@
 // Handles loading the events for <model-viewer>'s slotted progress bar
  <script type="module" src="https://ajax.googleapis.com/ajax/libs/model-viewer/4.3.1/model-viewer.min.js"></script>
 
-const skyboxViewer = document.querySelector('#skybox-bg');
-let angle = 0;
+customElements.whenDefined('model-viewer').then(() => {
+  const skyboxViewer = document.querySelector('#skybox-bg');
+  
+  if (!skyboxViewer) {
+    console.error('Skybox-Element nicht gefunden! Check die ID.');
+    return;
+  }
+  
+   if (!skyboxViewer) {
+    console.error('Skybox-Element nicht gefunden!');
+    return;
+  }
 
-setInterval(() => {
-  angle = (angle + 0.1) % 360;
-  skyboxViewer.cameraOrbit = `${angle}deg 75deg 0m`;
-}, 50);
+  let angle = 0;
+  setInterval(() => {
+    angle = (angle + 0.1) % 360;
+    skyboxViewer.cameraOrbit = `${angle}deg 75deg auto`;
+  }, 50);
+});
 
 
 const onProgress = (event) => {
