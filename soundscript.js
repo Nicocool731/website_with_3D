83faa8cfcd43150efa4c721fb98167d42943ff9e
Nicocool731 <kuzumi.sakuri731@gmail.com>
2026-07-24 15:04:45 +0200

    asdfd

--- soundscript.js
+++ soundscript.js
@@ -1,4 +1,20 @@
 
+
+const onProgress = (event) => {                                        //checking of models are loaded
+  const progressBar = event.target.querySelector('.progress-bar');
+  const updatingBar = event.target.querySelector('.update-bar');
+  updatingBar.style.width = `${event.detail.totalProgress * 100}%`;
+  if (event.detail.totalProgress === 1) {
+    progressBar.classList.add('hide');
+    event.target.removeEventListener('progress', onProgress);
+  } else {
+    progressBar.classList.remove('hide');
+  }
+};
+document.querySelector('model-viewer').addEventListener('progress', onProgress);
+
+
+
 const sound = new Audio("musik/psy.mp3");
 
 image.addEventListener('click', () => {
