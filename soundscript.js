a23481996aec4af6a8b388d192496a4e5eba36ae
Nicocool731 <kuzumi.sakuri731@gmail.com>
2026-07-24 15:06:39 +0200

    afdsa

--- soundscript.js
+++ soundscript.js
@@ -17,7 +17,7 @@ document.querySelector('model-viewer').addEventListener('progress', onProgress);
 
 const sound = new Audio("musik/psy.mp3");
 
-image.addEventListener('click', () => {
+sound.addEventListener('click', () => {
     sound.currentTime = 0;
     sound.play().catch(error => {
         console.error("The sound could not be played:", error);
