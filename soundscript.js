bbc3fe38bfb897e821f62b9bfb6e3cfcc71c6433
Nicocool731 <kuzumi.sakuri731@gmail.com>
2026-07-24 14:59:28 +0200

    adfdas

--- soundscript.js
+++ soundscript.js
@@ -1,12 +1,9 @@
 
-
 const sound = new Audio("musik/psy.mp3");
 
 image.addEventListener('click', () => {
     sound.currentTime = 0;
-    sound.play();
     sound.play().catch(error => {
         console.error("The sound could not be played:", error);
-    }); 
+    });
 });
-
